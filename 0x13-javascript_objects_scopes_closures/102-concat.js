27b1e5c290fec8e136e4a4686d1f8e723d2f6cef
nader777 <naderfekry3@gmail.com>
2024-04-09 13:28:22 -0700

    second-fix

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
+// Concatenates file passd as arguments into output file, also arg.
 const fs = require('fs');
-
-const fArg = fs.readFileSync(process.argv[2]).toString();
-const sArg = fs.readFileSync(process.argv[3]).toString();
-fs.writeFileSync(process.argv[4], fArg + sArg);
+const data1 = fs.readFileSync(process.argv[2], 'utf-8');
+const data2 = fs.readFileSync(process.argv[3], 'utf-8');
+fs.writeFileSync(process.argv[4], data1 + data2, 'utf-8');
