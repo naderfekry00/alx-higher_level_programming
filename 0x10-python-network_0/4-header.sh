5219c5b3f982addb05b7f1b6824a77887f3a3a0f
nader777 <naderfekry3@gmail.com>
2024-04-26 11:29:12 -0700

    fix

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script to send GET request with header var X-HolbertonSchool-User-Id val 98
-curl -s "$1" -X GET -H "X-HolbertonSchool-User-Id:98"
+# Send a GET request to a given URL with a header variable.
+curl -s -H "X-School-User-Id":98 "$1"
